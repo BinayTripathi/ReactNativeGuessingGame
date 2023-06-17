090b823c5647038f9a93a941eb419b6ee96e1b75
Binay <binay.mckv@gmail.com>
2023-06-17 11:41:31 +1000

    First commit without gameover

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  ImageBackground, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import StartGameScreen from './Screens/StartGameScreen'
+import GameSrceen from './Screens/GameScreen';
+import Colors from './Constants/Colors';
+import GameOverScreen from './Screens/GameoverScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+  const [enteredNumber, setEnteredNumber] = useState();
+  const [gameIsOver, setGameIsOver] = useState(true);
+
+  const pickedNumberHandler = (userEnteredNumber) => {
+    setEnteredNumber(userEnteredNumber)
+    setGameIsOver(false);
+  } 
+
+  const gameOverHandler = () => setGameIsOver(true)
+  
+  let  screen = (<StartGameScreen onPickNumber={pickedNumberHandler}/>)
+  if(enteredNumber)
+    screen = (<GameSrceen  userNumber={enteredNumber} onGameOver={gameOverHandler}/>)
+
+  if (gameIsOver && enteredNumber) {
+    screen = <GameOverScreen />;
+  }
+
+  return (   
+      <LinearGradient style = {styles.rootScreen} colors={[Colors.primary700, Colors.accent500]}>
+        <ImageBackground
+          source={require('./assets/images/background.png')}
+          resizeMode='cover'
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}> 
+            <SafeAreaView>{screen}</SafeAreaView>
+        </ImageBackground>       
+      </LinearGradient>  
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  rootScreen: {
+    flex: 1,  
   },
+  backgroundImage: {
+    opacity: 0.15,    
+  }
 });
